eda48e8a8b17bc723811c31e9f507716d20656f1
ivan <vanamelcikov7275@gmail.com>
2025-06-23 23:20:18 +0400

    add: FilePicer component

---
+++ src/features/FilePicker.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default {
+  props: {
+    filename: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<template>
+  <img src="/images/pdf_ico.png" width="25" height="25" alt="pdf" />
+  <a :href="filename" target="_blank">{{ title }}</a>
+</template>
